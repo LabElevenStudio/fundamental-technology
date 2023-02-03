ef23729dd76124317307101b729e3b1959286c92
hill <hillonyechekwa@gmail.com>
2023-02-03 13:23:39 +0100

    created mobile view for l-mobile screens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,56 @@
-import styles from './Footer.module.css'
+import Styles from '@styles/footer.module.scss'
+import {FaFacebookF, FaTwitter, FaInstagram} from 'react-icons/fa'
+
+
+
 
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      <footer className={Styles.footer}>
+        <div className={Styles.footerContent}>
+          <div className={Styles.footerInner}>
+            <img src="/images/logo-white.svg" alt="Funda Paints Logo" className="logo" />
+            <ul>
+              <li>
+                <FaFacebookF />
+              </li>
+              <li>
+                <FaTwitter />
+              </li>
+              <li>
+                <FaInstagram />
+              </li>
+            </ul>
+          </div>
+          <div className={Styles.footerNav}>
+            <label htmlFor="footer-contact">
+              <p>Stay up to date on the latest from Fundamental Technology</p>
+              <span className={Styles.formInput}>
+                <input type="text" id="footer-contact" />
+                <button type="submit"> Submit </button>
+              </span>
+            </label>
+            <nav className={Styles.footerNavInner}>
+              <dl>
+                <dt>Support</dt>
+                <dd>Contact Us</dd>
+                <dd>FAQ</dd>
+                <dd>Consultation</dd>
+              </dl>
+              <dl>
+                <dt>Fundamental Technology</dt>
+                <dd>About Us</dd>
+                <dd>Our Products</dd>
+                <dd>Newsroom</dd>
+              </dl>
+            </nav>
+          </div>
+        </div>
+        <div className={Styles.footerBottom}>
+          <p>Copyright &copy; {new Date().getFullYear()} Funda Technology</p>
+          <p>Powered by Funda Technology</p>
+        </div>
       </footer>
     </>
   )
